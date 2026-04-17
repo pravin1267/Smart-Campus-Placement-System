f9b60b8e31fe988b1a46cee5d375c51eb1a1634a
Pravin Kshirsagar <kshirsagarpravin847@gmail.com>
2026-04-17 22:05:39 +0530

    Removed target folder

--- src/main/java/com/campusplacement/dao/DBConnection.java
+++ src/main/java/com/campusplacement/dao/DBConnection.java
@@ -5,24 +5,30 @@ import java.sql.DriverManager;
 
 public class DBConnection {
 
-	private static final String URL =
-		    "jdbc:mysql://" + System.getenv("MYSQLHOST") + ":" +
-		    System.getenv("MYSQLPORT") + "/" +
-		    System.getenv("MYSQLDATABASE") +
-		    "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+    public static Connection getConnection() {
+        try {
+            // Load JDBC Driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
 
-		private static final String USER = System.getenv("MYSQLUSER");
-		private static final String PWD = System.getenv("MYSQLPASSWORD");
+            // Get values from environment variables
+            String URL = "jdbc:mysql://" 
+                + System.getenv("MYSQLHOST") + ":" 
+                + System.getenv("MYSQLPORT") + "/" 
+                + System.getenv("MYSQLDATABASE") 
+                + "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
 
-	public static Connection getConnection() {
-		try {
-			return DriverManager.getConnection(URL, USER, PWD);
-             System.out.println("JDBC URL: " + URL);
-	        System.out.println("USER: " + USER);
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
+            String USER = System.getenv("MYSQLUSER");
+            String PWD  = System.getenv("MYSQLPASSWORD");
+
+            // Debug logs (optional)
+            System.out.println("JDBC URL: " + URL);
+            System.out.println("USER: " + USER);
+
+            return DriverManager.getConnection(URL, USER, PWD);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
