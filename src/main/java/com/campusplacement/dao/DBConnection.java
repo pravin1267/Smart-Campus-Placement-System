96b8178f91716055125bf203c5f3e7171ea8b261
Pravin Kshirsagar <kshirsagarpravin847@gmail.com>
2026-04-17 18:32:45 +0530

    Fixed DB connection debug

--- src/main/java/com/campusplacement/dao/DBConnection.java
+++ src/main/java/com/campusplacement/dao/DBConnection.java
@@ -4,7 +4,15 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBConnection {
+	public static Connection getConnection() {
+	    try {
+	        String url = "jdbc:mysql://" 
+	            + System.getenv("MYSQLHOST") + ":" 
+	            + System.getenv("MYSQLPORT") + "/" 
+	            + System.getenv("MYSQLDATABASE") 
+	            + "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
 
+<<<<<<< HEAD
     public static Connection getConnection() {
         try {
             // Load JDBC Driver
@@ -25,10 +33,22 @@ public class DBConnection {
             System.out.println("USER: " + USER);
 
             return DriverManager.getConnection(URL, USER, PWD);
+=======
+	        String user = System.getenv("MYSQLUSER");
+	        String pwd  = System.getenv("MYSQLPASSWORD");
 
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
+	        // ✅ FIXED
+	        System.out.println("JDBC URL: " + url);
+	        System.out.println("USER: " + user);
+
+	        Class.forName("com.mysql.cj.jdbc.Driver");
+	        return DriverManager.getConnection(url, user, pwd);
+>>>>>>> 891f63d (Fixed DB connection debug)
+
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	        return null;
+	    }
+	
     }
 }
